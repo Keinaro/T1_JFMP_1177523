75510b30ef43f183c8e2c4bf7f3ea264d296b6c7
georg <georg@LAPTOP-FAM-MUNO>
2023-02-18 11:58:59 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mi Segundo Programa");
+        Console.ReadKey();
+
+        Console.Write("\nIngrese su Nombre: ");
+            string Nombre = Console.ReadLine();
+
+        Console.Write("Ingrese su Edad: ");
+            string Edad = Console.ReadLine();
+
+        Console.Write("Ingrese su Carrera: ");
+            string Carrera = Console.ReadLine();
+
+        Console.Write("Ingrese su Carné: ");
+            string Carné = Console.ReadLine();
+
+        Console.Write("\nSoy " + Nombre);
+        Console.Write(",tengo " + Edad);
+        Console.Write(" años y estudio la carrera de " + Carrera);
+        Console.Write(". Mi número de carné es; " + Carné);
+        Console.ReadKey();
+
+    }
+}
